7d4cfd764145ed11bb66ae88b162378345155ed6
jakepscott <jakepscott16@gmail.com>
2021-06-12 18:50:44 -0400

    Add input toggler

--- 02_Dashboard.R
+++ 02_Dashboard.R
@@ -22,15 +22,23 @@ theme_set(theme_minimal(base_family = "Roboto Condensed",
                         base_size=12))
 
 # Loading Necessary Data and Functions ------------------------------------
+
 #Spotify API key
 source(here("key.R"))
-#Load my data
-data <- read_rds(here("data/full_data.rds"))
-#Load all songs plot function
-source(here("helper-functions/all_songs_analysis/AllSongs_Plot_Function.R"))
-#Load helpful functions
-source(here("helper-functions/Barplot_Function.R"))
-source(here("helper-functions/Scatterplot_Function.R"))
+
+#Load liked songs data
+liked_songs <- read_rds(here("data/full_data.rds"))
+
+#Load playlist data
+playlists <- read_rds(here("data/playlist_tracks.rds"))
+
+#Load helper functions
+source(here("helper-functions/all_songs_analysis/AllSongs_Plot_Function.R"))#Load all songs plot function
+source(here("helper-functions/playlists_analysis/Playlist_Plot.R")) #Load the playlists plot function
+source(here("helper-functions/Barplot_Function.R"))#Load barplot function
+source(here("helper-functions/Scatterplot_Function.R")) #Load scatterplot function
+source(here("helper-functions/Aggregate_Function.R")) #Load playlist aggregation function
+source(here("helper-functions/Input_Toggle.R"))
 
 #Mandatory Fields
 fieldsMandatory <- c("username", "playlists","features")
@@ -42,8 +50,9 @@ RV <- reactiveValues()
 
 
 ui <- dashboardPage(skin = "green",
+                    
                     # Dashboard Header --------------------------------------------------------
-                    dashboardHeader(title = "Spotify Song Analysis"),
+                    dashboardHeader(title = "Spotify Analysis"),
                     
                     # Dashboard Sidebar -------------------------------------------------------  
                     dashboardSidebar(
@@ -57,6 +66,7 @@ ui <- dashboardPage(skin = "green",
                     
                     # Dashboard Body ----------------------------------------------------------
                     dashboardBody(
+                      setSliderColor("#1DB954", 2),
                       tabsetPanel(
                         # Overview tab -------------------------------------------------------------
                         tabPanel(title = "Liked Songs",
@@ -85,7 +95,6 @@ ui <- dashboardPage(skin = "green",
                                  # Mainbody format ---------------------------------------------------------
                                  fluidRow(),
                                  fluidRow(
-                                   setSliderColor("#1DB954", 1),
                                    #Input Selection
                                    column(width = 4, style='padding-left:0px',
                                           box(width = 12,
@@ -116,7 +125,38 @@ ui <- dashboardPage(skin = "green",
                         ),
                         
                         # Compare Playlists Tab ---------------------------------------------------
-                        tabPanel(title = "Analyze a Playlist")
+                        tabPanel(title = "Analyze a Playlist",
+                                 # Mainbody format ---------------------------------------------------------
+                                 fluidRow(),
+                                 fluidRow(
+                                   #setSliderColor("#1DB954", sliderId = 1:10),
+                                   #Input Selection
+                                   column(width = 4, style='padding-left:0px',
+                                          box(width = 12,
+                                              selectizeInput("main_variable_playlist",label="Main Feature (for bar plot and y-axis of scatter)",
+                                                             choices = c("Danceability"="danceability", "Date Song Saved"="Date_Song_Saved",
+                                                                         "Duration"="minutes", "Energy"="energy", "Loudness"="loudness", 
+                                                                         "Tempo"="tempo", "Track Popularity"="track_popularity", 
+                                                                         "Track Release Date"="Track_Release_Date",  "Valence"="valence"),
+                                                             selected="danceability"),
+                                              selectizeInput("comp_variable_playlist",label="Comparison Feature (for x-axis of scatter)",
+                                                             choices = c("Danceability"="danceability", "Date Song Saved"="Date_Song_Saved",
+                                                                         "Duration"="minutes", "Energy"="energy", "Loudness"="loudness", 
+                                                                         "Tempo"="tempo", "Track Popularity"="track_popularity", 
+                                                                         "Track Release Date"="Track_Release_Date",  "Valence"="valence"),
+                                                             selected="valence"),
+                                              sliderInput(inputId = "num_tracks_barplot_playlist", label = "How many tracks to show in bar plot (top and bottom):",
+                                                          max = 10, min=3, value = 5, 
+                                                          step = 1, round = T),
+                                              actionButton("playlists_plot_go","View")
+                                              
+                                          )),
+                                   # #Figure
+                                   # column(width = 8, style='padding-left:0px', 
+                                   #        box(width=12,
+                                   #            withSpinner(girafeOutput("All_Songs_Plot"),type = 6,color = "#1DB954")
+                                   #        ))
+                                 ))
                       )
                     )
 )
@@ -126,18 +166,16 @@ ui <- dashboardPage(skin = "green",
 
 
 server <- function(input, output, session) {
-  ## initialize reactive values
-  obj <- reactiveValues()
-  
-  #Initially disable all functions until a username has been found
-  shinyjs::disable("playlists")
+  ## initialize reactive values, which is a named list that will hold my data objects
+  data <- reactiveValues()
   
   #Disable exploration UI until data is collected
-  shinyjs::disable("main_variable_allsongs")
-  shinyjs::disable("comp_variable_allsongs")
-  shinyjs::disable("num_tracks_barplot_allsongs")
+  input_toggle(enable_or_disable = "disable")
   
-  #generating playlist selection function -------------------------------------------------------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Obtain data from username ---------------------------------------------------------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #Finding username, collecting data, enabling inputs  -------------------------
   observeEvent(input$usernameclick, {
     #This makes it so that we just have the username of the user
     if (str_detect(input$username,"spotify")==T) {
@@ -153,41 +191,76 @@ server <- function(input, output, session) {
                            choices = "")
     } else {
       shinyalert("Success!", "Username found", type = "success")
-      #Enabling playlist and feature selection if and only if a username is found
+      
+      #Enabling exploration UI if and only if a username is found
       #Now that data is loaded, enable the exploration UI
-      shinyjs::enable("main_variable_allsongs")
-      shinyjs::enable("comp_variable_allsongs")
-      shinyjs::enable("num_tracks_barplot_allsongs")
+      input_toggle(enable_or_disable = "enable")
       
       #Saving the data into a reactive object housed in obj
-      obj$full_data <- data
+      data$liked_songs <- liked_songs
+      data$playlists <- playlists 
     }
   })
   
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # All Songs Bar Plot ---------------------------------------------------------
+  # All Songs Plot ---------------------------------------------------------
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
+  #Initialize a reactive values list, which each entry being one of the inputs for the all songs 
+  # function, set to NUL
   all_songs_plot_inputs<- reactiveValues(main_variable=NULL,
                                          comparison_variable=NULL,
                                          num_tracks=NULL,
                                          data=NULL)
+  
+  #Once the "View" button is clicked, set the value of each of those entries within all_songs_plot_inputs to the 
+  # user defined value
   observeEvent(input$all_songs_plot_go, {
     all_songs_plot_inputs$main_variable <- input$main_variable_allsongs
     all_songs_plot_inputs$comparison_variable <- input$comp_variable_allsongs
     all_songs_plot_inputs$num_tracks <- input$num_tracks_barplot_allsongs
-    all_songs_plot_inputs$data <- obj$full_data
+    all_songs_plot_inputs$liked_songs <- data$liked_songs
   })
   
+  #Plug the inputs from all_songs_plot_inputs into the AllSongs_Plot function
   output$All_Songs_Plot <- renderGirafe({
-    req(!is.null(all_songs_plot_inputs$data))
+    req(!is.null(all_songs_plot_inputs$liked_songs))
     AllSongs_Plot(main_variable = all_songs_plot_inputs$main_variable, 
                   comparison_variable = all_songs_plot_inputs$comparison_variable,
                   how_many = all_songs_plot_inputs$num_tracks, 
-                  data = all_songs_plot_inputs$data)
+                  data = all_songs_plot_inputs$liked_songs)
     #}  
     })
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Playlists Plot ---------------------------------------------------------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #Initialize a reactive values list, which each entry being one of the inputs for the all songs 
+  # function, set to NUL
+  playlists_plot_inputs<- reactiveValues(main_variable=NULL,
+                                         comparison_variable=NULL,
+                                         num_tracks=NULL,
+                                         data=NULL)
+  
+  #Once the "View" button is clicked, set the value of each of those entries within playlists_plot_inputs to the 
+  # user defined value
+  observeEvent(input$all_songs_plot_go, {
+    playlists_plot_inputs$main_variable <- input$main_variable_playlist
+    playlists_plot_inputs$comparison_variable <- input$comp_variable_playlist
+    playlists_plot_inputs$num_tracks <- input$num_tracks_barplot_playlist
+    playlists_plot_inputs$liked_songs <- data$playlists
+  })
+  
+  #Plug the inputs from playlists_plot_inputs into the AllSongs_Plot function
+  output$All_Songs_Plot <- renderGirafe({
+    req(!is.null(playlists_plot_inputs$playlists))
+    (main_variable = playlists_plot_inputs$main_variable, 
+                  comparison_variable = playlists_plot_inputs$comparison_variable,
+                  how_many = playlists_plot_inputs$num_tracks, 
+                  data = playlists_plot_inputs$playlists)
+    #}  
+  })
+  
 }
 
 shinyApp(ui, server)
---
+++ helper-functions/Input_Toggle.R
@@ -0,0 +1,26 @@
+input_toggle <- function(enable_or_disable){
+  if (enable_or_disable=="disable") {
+    #Disable exploration UI until data is collected
+    #Disable All Songs Tab
+    shinyjs::disable("main_variable_allsongs")
+    shinyjs::disable("comp_variable_allsongs")
+    shinyjs::disable("num_tracks_barplot_allsongs")
+    shinyjs::disable("all_songs_plot_go")
+    #Disable Playlist Tab
+    shinyjs::disable("main_variable_playlist")
+    shinyjs::disable("comp_variable_playlist")
+    shinyjs::disable("num_tracks_barplot_playlist")
+    shinyjs::disable("playlists_plot_go")
+  } else if (enable_or_disable=="enable") {
+    #All Songs UI
+    shinyjs::enable("main_variable_allsongs")
+    shinyjs::enable("comp_variable_allsongs")
+    shinyjs::enable("num_tracks_barplot_allsongs")
+    shinyjs::enable("all_songs_plot_go")
+    #Playlists UI
+    shinyjs::enable("main_variable_playlist")
+    shinyjs::enable("comp_variable_playlist")
+    shinyjs::enable("num_tracks_barplot_playlist")
+    shinyjs::enable("playlists_plot_go")
+  }
+}
