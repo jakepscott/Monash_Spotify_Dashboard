1e5a68de8ebdef944b62b9aca4187e84f4d8c8b2
jakepscott <jakepscott16@gmail.com>
2021-06-12 17:45:01 -0400

    Add aggregation method section to playlist plot

--- helper-functions/playlists_analysis/Playlist_Plot.R
+++ helper-functions/playlists_analysis/Playlist_Plot.R
@@ -4,6 +4,7 @@ library(glue)
 library(lubridate)
 library(ggiraph)
 library(ggtext)
+library(patchwork)
 theme_set(theme_minimal(base_size = 12))
 
 playlist_tracks <- read_rds(here("data/playlist_tracks.rds"))
@@ -14,15 +15,33 @@ comparison_variable <- "Date_Song_Saved"
 how_many <- 5
 data <- playlist_tracks
 
-if (method=="compare_playlists") {
-  data_agg <- aggregate_data(main_variable = main_variable, 
-                             comparison_variable = comparison_variable,
-                             data=data)
-  barplot <- barplot_function(main_variable = main_variable,
-                              how_many = how_many,
-                              data = data_agg)
-  scatterplot <- scatterplot_function(main_variable = main_variable,
-                                      comparison_variable = comparison_variable,
-                                      data = data_agg)
+playlist_plot <- function(method, main_variable, comparison_variable, 
+                          how_many, data){
+  #If the method chosen is to aggregate and compare across playlists
+  if (method=="compare_playlists") {
+    #Aggregate the data
+    data_agg <- aggregate_data(main_variable = main_variable, 
+                               comparison_variable = comparison_variable,
+                               data=data)
+    #Create the bar plot
+    barplot <- barplot_function(main_variable = main_variable,
+                                how_many = how_many,
+                                data = data_agg)
+    #Create the scatter plot
+    scatterplot <- scatterplot_function(main_variable = main_variable,
+                                        comparison_variable = comparison_variable,
+                                        data = data_agg)
+    #Join bar and scatter plot
+    bar_and_scatter <- barplot + scatterplot + plot_layout(ncol = 1)
+    
+    #Make the plots interactive
+    girafe(ggobj = bar_and_scatter,
+           options = list(
+             opts_selection(type = "multiple",only_shiny = FALSE),
+             opts_hover(css = "fill:#1DB954;"),
+             opts_hover_inv(css = "opacity:0.25;")))
+    
+  }
   
 }
+
