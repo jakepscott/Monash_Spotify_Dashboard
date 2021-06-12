04269d2ff3c070e87529a89868adb482fd962231
jakepscott <jakepscott16@gmail.com>
2021-06-12 17:34:39 -0400

    Initial commit for playlist plot function

--- helper-functions/playlists_analysis/Playlist_Plot.R
+++ helper-functions/playlists_analysis/Playlist_Plot.R
@@ -1,14 +1,28 @@
 library(tidyverse)
 library(here)
+library(glue)
+library(lubridate)
+library(ggiraph)
+library(ggtext)
+theme_set(theme_minimal(base_size = 12))
 
 playlist_tracks <- read_rds(here("data/playlist_tracks.rds"))
 
-aggregation <- "compare_playlists"
-main_variable <- "Track_Release_Date"
-comparison_variable <- "minutes"
+method <- "compare_playlists"
+main_variable <- "valence"
+comparison_variable <- "Date_Song_Saved"
 how_many <- 5
-data <- playlist_tracks_features
+data <- playlist_tracks
 
-if (aggregation=="compare_playlists") {
+if (method=="compare_playlists") {
+  data_agg <- aggregate_data(main_variable = main_variable, 
+                             comparison_variable = comparison_variable,
+                             data=data)
+  barplot <- barplot_function(main_variable = main_variable,
+                              how_many = how_many,
+                              data = data_agg)
+  scatterplot <- scatterplot_function(main_variable = main_variable,
+                                      comparison_variable = comparison_variable,
+                                      data = data_agg)
   
 }
